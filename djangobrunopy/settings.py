f76a1b625faf09b07998a4d0a688ca43e2473ae5
brjatoba92 <brjatoba92@gmail.com>
2020-10-07 16:06:08 -0300

    Implementando secret key
    close #17

--- djangobrunopy/settings.py
+++ djangobrunopy/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j)govuz%dv-5eheyr4w-!-e0n(*vyv+9&nlb4)*f!pai8p9g!_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
